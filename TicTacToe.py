d1d19ea439154173b72ee513ab9c5eaed469844a
joleon88 <joseortegaleon88@gmail.com>
2022-09-22 15:55:25 -0500

    Visual Tictactoe de user contra PC

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,10 +1,14 @@
 import random
-
+import sys, pygame
+import numpy as np
 
 class TicTacToe:
 
     def __init__(self):
         self.board = []
+        self.create_board()
+        self.player = 'X' if self.get_random_first_player() == 1 else 'O'
+        self.playerPC = "O" if self.player == "X" else 'X'
 
     def create_board(self):
         for i in range(3):
@@ -19,15 +23,9 @@ class TicTacToe:
     def fix_spot(self, row, col, player):
         if self.board[row][col] == '-':
             self.board[row][col] = player
+            return True
         else:
-            print(f"Player {player} esa casilla está ocupada.")
-            print(f"Player {player} turn")
-            self.show_board()
-            # taking user input
-            row, col = list(
-                map(int, input("Enter row and column numbers to fix spot: ").split()))
-            print()
-            self.fix_spot(row-1, col-1, player)
+           return False
 
     def estado_favorable(self, board, playerPC):
         #Ver si la PC gana en su proxima Jugada
@@ -50,6 +48,8 @@ class TicTacToe:
         jugada=self.elegir_jugada_azar(board, jugadasGood)
         if jugada != None:
             return jugada
+        else:
+            return None, None
         #Intentar coger el centro
         if board[2][2] == "-":
             return 2, 2
@@ -75,7 +75,7 @@ class TicTacToe:
         if len(jugadas) != 0:
             return random.choice(jugadas)
         else:
-            None
+            return None
 
     def is_player_winPC(self, board, player):
         win = None
@@ -211,54 +211,167 @@ class TicTacToe:
                 print(item, end=" ")
             print()
 
-    def start(self):
-        self.create_board()
 
-        player = 'X' if self.get_random_first_player() == 1 else 'O'
-        playerPC = "O" if player == "X" else 'X'
-        while True:
-            print(f"Player {player} turn")
+class BollGame:
 
-            self.show_board()
+    def __init__(self):
+        # Inicializamos pygame
+        pygame.init()
+        # Muestro una ventana de 800x600
+        self.size = 360, 360
+        self.screen = pygame.display.set_mode(self.size)
+        # Cambio el título de la ventana
+        pygame.display.set_caption("Juego Tic-Tac-Toe")
+        # Inicializamos variables
+        self.width, self.height = 360, 360
+        # Crea un objeto imagen tablero y obtengo su rectángulo
+        self.tablero = pygame.image.load("tablero.png")
+        self.tablerorect = self.tablero.get_rect()
+        # Pongo el tablero en la pantalla
+        self.tablerorect.move_ip(0, 0)
+        # Crea un objeto imagen juador X y obtengo su rectángulo
+        self.xPlayer = pygame.image.load("X.png")
+        self.xrect = self.xPlayer.get_rect()
+        # Crea un objeto imagen juador O y obtengo su rectángulo
+        self.oPlayer = pygame.image.load("O.png")
+        self.orect = self.oPlayer.get_rect()
+        self.run=True
+        self.diclic = False
+        self.juegaPC = False
+        self.termino=False
+        #inicializo TicTacToe
+        self.tic_tac_toe = TicTacToe()
 
-            # taking user input
-            row, col = list(
-                map(int, input("Enter row and column numbers to fix spot: ").split()))
-            print()
 
-            # fixing the spot
-            self.fix_spot(row - 1, col - 1, player)
 
-            # checking whether current player is won or not
-            if self.is_player_win(player):
-                print(f"Player {player} wins the game!")
-                break
+    def startGame(self):
+        while self.run:
+            # Espero un tiempo (milisegundos) para que la pelota no vaya muy rápida
+            pygame.time.delay(1)
+            # Capturamos los eventos que se han producido
+            for event in pygame.event.get():
+                # Si el evento es salir de la ventana, terminamos
+                if event.type == pygame.QUIT: self.run = False
 
-            # checking whether the game is draw or not
-            if self.is_board_filled():
-                print("Match Draw!")
-                break
+            row, col =0, 0
+            if event.type == pygame.MOUSEBUTTONDOWN and not self.termino and not self.juegaPC:
+                xPos, yPos = pygame.mouse.get_pos()
+                if (0 <= xPos <= 120) and (0 <= yPos <= 120):
+                    xPos, yPos = 0, 0
+                    row, col = 1, 1
+                elif (120+1 <= xPos <= 240) and (0 <= yPos <= 120):
+                    xPos, yPos = 120, 0
+                    row, col = 1, 2
+                elif (240+1 <= xPos <= 360) and (0 <= yPos <= 120):
+                    xPos, yPos = 240, 0
+                    row, col = 1, 3
+                elif (0 <= xPos <= 120) and (120+1 <= yPos <= 240):
+                    xPos, yPos = 0, 120
+                    row, col = 2, 1
+                elif (120+1 <= xPos <= 240) and (121 <= yPos <= 240):
+                    xPos, yPos = 120, 120
+                    row, col = 2, 2
+                elif (240+1 <= xPos <= 360) and (121 <= yPos <= 240):
+                    xPos, yPos = 240, 120
+                    row, col = 2, 3
+                elif (0 <= xPos <= 120) and (241 <= yPos <= 360):
+                    xPos, yPos = 0, 240
+                    row, col = 3, 1
+                elif (120+1 <= xPos <= 240) and (241 <= yPos <= 360):
+                    xPos, yPos = 120, 240
+                    row, col = 3, 2
+                elif (240+1 <= xPos <= 360) and (241 <= yPos <= 360):
+                    xPos, yPos = 240, 240
+                    row, col = 3, 3
+                print(xPos, yPos)
+                print("Evento solte el clic")
+                # Pinto la X o O y actualizo la pantalla
+                if self.tic_tac_toe.player == "X":
+                    self.xrect.update(self.xPlayer.get_rect())
+                    self.xrect.move_ip(xPos, yPos)
+                else:
+                    self.orect.update(self.oPlayer.get_rect())
+                    self.orect.move_ip(xPos, yPos)
+                # Hago la logica para jugar
+                self.juegaPC = self.tic_tac_toe.fix_spot(row - 1, col - 1, self.tic_tac_toe.player)
+                if self.tic_tac_toe.is_player_win(self.tic_tac_toe.player):
+                    print(f"Player {self.tic_tac_toe.player} wins the game!")
+                    self.termino=True
+                # checking whether the game is draw or not
+                if self.tic_tac_toe.is_board_filled():
+                    print("Match Draw!")
+                    self.termino=True
 
-            #poner a la PC a Jugar
-            print(f"Ya jugo Player {player}!")
-            print(f"Ahora juega PlayerPC {playerPC}!")
-            fila, columna = self.estado_favorable(self.board, playerPC)
-            print(f"La PC jugo {fila} {columna}")
-            self.board[fila-1][columna-1] = playerPC
+                # Poner la PC a Jugar
+                if self.juegaPC and not self.termino:
+                    print(f"Ya jugo Player {self.tic_tac_toe.player}!")
+                    print(f"Ahora juega PlayerPC {self.tic_tac_toe.playerPC}!")
+                    fila, columna = self.tic_tac_toe.estado_favorable(self.tic_tac_toe.board,self.tic_tac_toe.playerPC)
+                    if fila != None and columna != None:
+                        print(f"La PC jugo {fila} {columna}")
+                        self.tic_tac_toe.board[fila - 1][columna - 1] = self.tic_tac_toe.playerPC
+                        # checking whether playerPC is won or not
+                        if self.tic_tac_toe.is_player_win(self.tic_tac_toe.playerPC):
+                            print(f"Player {self.tic_tac_toe.playerPC} wins the game!")
+                            self.termino = True
+                    else:
+                        print("Match Draw!")
+                        self.termino=True
 
-            # checking whether playerPC is won or not
-            if self.is_player_win(playerPC):
-                print(f"Player {playerPC} wins the game!")
-                break
+                self.juegaPC = False
+            self.diclic = True
 
-            # swapping the turn
-            #player = self.swap_player_turn(playerPC)
 
-        # showing the final view of board
-        print()
-        self.show_board()
+
+
+            # Pinto el fondo de blanco, dibujo la pelota y actualizo la pantalla
+            self.screen.blit(self.tablero, self.tablerorect)
+            for i in range(3):
+                for j in range(3):
+                    xPos, yPos = self.damePosTablero(i, j)
+                    if self.tic_tac_toe.board[i][j] == "X":
+                        self.xPlayer = pygame.image.load("X.png")
+                        self.xrect.update(self.xPlayer.get_rect())
+                        self.xrect.move_ip(xPos, yPos)
+                        self.screen.blit(self.xPlayer, self.xrect)
+                    if self.tic_tac_toe.board[i][j] == "O":
+                        self.oPlayer = pygame.image.load("O.png")
+                        self.orect.update(self.oPlayer.get_rect())
+                        self.orect.move_ip(xPos, yPos)
+                        self.screen.blit(self.oPlayer, self.orect)
+
+
+            pygame.display.flip()
+        # Salgo de pygame
+        pygame.quit()
+
+
+    def damePosTablero(self, i, j):
+        xPos, yPos = None, None
+        if (i == 0) and (j == 0):
+           xPos , yPos = 0, 0
+        elif (i == 0) and (j == 1):
+            xPos, yPos = 120, 0
+        elif (i == 0) and (j == 2):
+            xPos, yPos = 240, 0
+        elif (i == 1) and (j == 0):
+            xPos, yPos = 0, 120
+        elif (i == 1) and (j == 1):
+            xPos, yPos = 120, 120
+        elif (i == 1) and (j == 2):
+            xPos, yPos = 240, 120
+        elif (i == 2) and (j == 0):
+            xPos, yPos = 0, 240
+        elif (i == 2) and (j == 1):
+            xPos, yPos = 120, 240
+        elif (i == 2) and (j == 2):
+            xPos, yPos = 240, 240
+        return xPos, yPos
+
 
 
 # starting the game
-tic_tac_toe = TicTacToe()
-tic_tac_toe.start()
+boll_Game = BollGame()
+boll_Game.startGame()
+#tic_tac_toe = TicTacToe()
+#tic_tac_toe.start()
